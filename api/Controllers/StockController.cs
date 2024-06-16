26dfdf3051a988b3b15d4da2cf8c4400aab10ea7
IvyZayn <kakapon713@outlook.com>
2024-06-16 21:27:47 +0100

    Add delete endpoint

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -72,5 +72,23 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null) 
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
