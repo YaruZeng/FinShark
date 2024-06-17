998c505fd91c36814237b1515bd173644d096c3e
IvyZayn <kakapon713@outlook.com>
2024-06-17 14:50:39 +0100

    build comment repo and get enpoints

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -41,12 +41,12 @@ namespace api.Repository
 
         public async Task<List<Stock>> GetAllAsync()
         {
-            return await _context.Stocks.ToListAsync();
+            return await _context.Stocks.Include(c => c.Comments).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
         {
-            return await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            return await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
