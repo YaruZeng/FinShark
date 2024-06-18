baa9c12650cfd637069314f53647c817991fda23
IvyZayn <kakapon713@outlook.com>
2024-06-18 21:24:10 +0100

    add register feature

---
+++ api/Migrations/20240618201835_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "12ad943c-32bb-48b0-b396-5238f71cc397", null, "Admin", "ADMIN" },
+                    { "6c067dbd-afb6-479d-9bcb-5703d645ff68", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "12ad943c-32bb-48b0-b396-5238f71cc397");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6c067dbd-afb6-479d-9bcb-5703d645ff68");
+        }
+    }
+}
