b414fe3f67f31eba870ead8c0cf4f41cc461c3e4
IvyZayn <kakapon713@outlook.com>
2024-06-20 14:28:38 +0100

    add user login, register, logout services to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Components/Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <UserProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
     </>
   );
 }
